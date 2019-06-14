963a073c2b009abda812c7d046c24ce802c58ab7
Josiane Dufour <josiane@Josianes-MacBook-Pro.local>
2019-06-14 14:59:45 +0100

    set up api request to fetch new friend

--- src/store/saga.js
+++ src/store/saga.js
@@ -1,10 +1,20 @@
-import { all, call, put, takeEvery } from 'redux-saga/effects';
+import { all, call, put, takeEvery } from "redux-saga/effects";
 
-import Api from '../lib/api';
+import Api from "../lib/api";
 
-import { REQUEST_NEW_FRIEND } from '../constants';
-import { addFriendToList } from '../actions/index';
+import { REQUEST_NEW_FRIEND } from "../constants";
+import { addFriendToList } from "../actions/index";
 
 export default function* rootSaga() {
+  yield all([fetchUserFromApi()]);
+}
 
+export function* fetchUserFromApi() {
+  yield takeEvery("REQUEST_NEW_FRIEND", makeApiRequest);
+}
+
+export function* makeApiRequest() {
+  const friend = yield call(Api.requestNewFriend);
+
+  yield put(addFriendToList(friends));
 }
